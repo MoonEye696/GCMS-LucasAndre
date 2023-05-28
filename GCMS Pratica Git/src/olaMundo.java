1c64d8fb5b1c34dcf507d2c7b2b5754db28aea57
lucas <lucas@DESKTOP-R4MHG14>
2023-05-28 20:42:42 -0300

    Alterações na classe olaMundo

---
+++ GCMS Pratica Git/src/olaMundo.java
@@ -0,0 +1,37 @@
+
+public class olaMundo {
+	//setando att aleatorios
+	private char letra;
+	private int numero;
+	
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo");
+		
+		
+	}
+
+
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+
+	public char getLetra() {
+		return letra;
+	}
+
+
+
+	public void setLetra(char letra) {
+		this.letra = letra;
+	}
+
+}
